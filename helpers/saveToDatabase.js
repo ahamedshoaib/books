2bf10e958c488bcb96738353e5ef0fc6e97ebc8e
Shoaib <ahamedshoaibk@gmail.com>
2018-02-06 08:05:38 +0530

    saveToDatabase: switched from bulkCreate to findCreateFind

--- helpers/saveToDatabase.js
+++ helpers/saveToDatabase.js
@@ -1,5 +1,21 @@
 const models = require('../models');
 
-const saveToDatabase = allBooks => models.books.bulkCreate(allBooks);
+// const saveToDatabase = allBooks => models.books.bulkCreate(allBooks);
+
+const saveToDatabase = (allBooks) => {
+  const promiseStoreDb = new Promise((resolve, reject) => {
+    const promises = [];
+    for (let bookNo = 0; bookNo < allBooks.length; bookNo += 1) {
+      promises.push(models.books.findCreateFind({
+        where: allBooks[bookNo],
+      }));
+    }
+    Promise.all(promises).then((values) => {
+      resolve({ statusCode: 200, message: 'sucsess' });
+    });
+  });
+
+  return promiseStoreDb;
+};
 
 module.exports = saveToDatabase;
